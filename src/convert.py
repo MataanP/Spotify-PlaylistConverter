e163d8afb808c8cab052460907e200df8f242538
Mataan Peer <mataanpeer@gmail.com>
2019-09-21 20:47:58 -0600

    added debugging

--- src/convert.py
+++ src/convert.py
@@ -54,9 +54,8 @@ class Converter():
             "Authorization": "Bearer "+token
         }
         response = requests.get(url,headers= headers)
-        reponse =  response.json()
-        print (response)
-        return response
+        print (response.text)
+        return response.json()
 
     def get_uri_of_new_tracks(self,tracks,token):
         song_uris =[]
